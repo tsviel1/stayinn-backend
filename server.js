f8aae1b33acb6ca8377b16a7ab020f90617ca21e
unknown <justsivan@gmail.com>
2022-07-26 22:08:59 +0300

    frontend connected to backend

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const http = require('http').createServer(app)
 app.use(cookieParser())
 app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
+    console.log(process.env.NODE_ENV)
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
+    console.log(process.env.NODE_ENV)
     const corsOptions = {
-        origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:5173'],
         credentials: true
     }
     app.use(cors(corsOptions))
